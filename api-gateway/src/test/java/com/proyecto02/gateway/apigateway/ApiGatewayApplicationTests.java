e8f48e0dac1a888debf8419e6a50352e9c364ad4
hans kennedy nolazco diaz <kenedy.97_nd@hotmail.com>
2023-02-08 17:48:18 -0500

    Proyecto ApiGateway

---
+++ api-gateway/src/test/java/com/proyecto02/gateway/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto02.gateway.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
